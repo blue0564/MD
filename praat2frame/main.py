6ff2cbe0cd272e467af7b59aeacb4d48972227fe
blue0564 <blue0564@nate.com>
2018-05-01 01:00:31 +0900

    remove praat2frame

--- praat2frame/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-sys.path.insert(0,'./data')	# insert folder saving 'time2class function files' to the system path
-from praat2frame import Time2Class
-
-a=Time2Class(inputTime=21,rttmFile="mask_00001_700K.rttm")
-print a
-
-
