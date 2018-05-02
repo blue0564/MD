12a9792c442fc4905f337724c14e17f1d25b190f
blue0564 <blue0564@nate.com>
2018-05-02 11:13:04 +0900

    Music Detection V1.0

--- SER/mapping_class.pl
+++ SER/mapping_class.pl
@@ -16,9 +16,6 @@ foreach (@configData) {
   $line = $_;
   @ref_class = $line =~ /:(\d+)-/;
   @map_class = $line =~ /-(\w+)\//;
-  #print "ref_class is ".$ref_class[0]."\n";
-  #print "map_class is ".$map_class[0]."\n";
-  #print "map_class size is ".$#map_class."\n";
   if($#ref_class eq 0 & $#map_class eq 0){
     push(@mapping,$map_class[0]);
   }
@@ -30,16 +27,38 @@ open(FILE, "<$ref_file") || die "mapping_class.pl, Error! open rttm\n";
 open(WFILE, ">$out_file") || die "mapping_class.pl, Error! open output\n";
 
 @data = <FILE>;
+$start=0;
+$preClass=0;
 foreach (@data) {
   $line=();
   $line=$_;
-  @file_name = $ref_file =~ /(\w+).rttm/;
 
-  @ref_class = $line =~ /<NA> (\d+) <NA>/;
+  #@str=$line=~/\s/;
+  @str=split / /,$line;
+  $nowStart=$str[3]; $nowDur=$str[4];
+  $nowEnd=$str[3] + $str[4];
+  $nowClass=$mapping[$str[7]-1];
+  $nowFile=$str[1];
+  #print WFILE "$str[0] $nowFile $str[2] $nowStart $nowDur $str[5] $str[6] $nowClass $str[8] $str[9]";
+  #print WFILE "preStart: $preStart, preDur: $preDur, preEnd: $preEnd, preClass: $preClass, preFile: $preFile\n";
+  #print WFILE "nowStart: $nowStart, nowDur: $nowDur, nowEnd: $nowEnd, nowClass: $nowClass, nowFile: $nowFile\n";
+  #$timeDiff=abs($preEnd - $nowStart)<0.002;
+  #print WFILE "diff Time: $timeDiff, \n";
+  #$preStart=$nowStart; $preDur=$nowDur; $preEnd=$nowEnd; $preClass=$nowClass; $preFile=$nowFile;
+
+  if(($nowClass eq $preClass) && (abs($preEnd-$nowStart)<0.002)){
+    #print WFILE "diff time is $preEnd-$nowStart\n";
+    $preDur=$preDur + $nowDur; $preEnd=$nowEnd; $preFile=$nowFile;
+    #print WFILE "preDur is $preDur\n";
+  }elsif(($start==1) && ($preFile eq $nowFile) && ($nowClass ne $preClass) | (abs($preEnd-$nowStart)>0.002) ){
+    print WFILE "$str[0] $preFile $str[2] $preStart $preDur $str[5] $str[6] $preClass $str[8] $str[9]";
+    $preStart=$nowStart; $preDur=$nowDur; $preEnd=$nowEnd; $preFile=$nowFile; $preClass=$nowClass;
+  }
  
-  $line =~ s/<NA> (\d+) <NA>/<NA> $mapping[$ref_class[0]-1] <NA>/;
+  #print WFILE "$line";
+  if($start==0){ $preStart=$nowStart; $preDur=$nowDur; $preEnd=$nowEnd; $preFile=$nowFile; $preClass=$nowClass; }
 
-  print WFILE "$line";
+  $start=1;
 
 }
 
