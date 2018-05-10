eb8c076fd7bfc8f6cf43918df3dbf43d6eabe5d6
blue0564 <blue0564@nate.com>
2018-05-10 14:14:04 +0900

    2018.05.10 modify Time2Class.py

--- Time2Class_test/Time2Class/Time2Class.py
+++ Time2Class_test/Time2Class/Time2Class.py
@@ -6,6 +6,11 @@
 # Input: input time, rttm file path to save
 # Output: class at the input time in reference
 #
+# modified 2018 May 2
+# if first time is bigger than 0,
+# modified 2018.05.10
+# add end time to time array to fix error
+#
 
 import subprocess
 import sys, os
@@ -33,6 +38,16 @@ def Read_rttm(rttmFile):
   f=open(rttmFile,"r")
   time=[];
   cla=[];
+  # modified 2018.05.03
+  line=f.readline()
+  words_line=line.split()
+  if float(words_line[3])>0:
+    time.append(float(0))
+    cla.append("SIL")
+
+  time.append(float(words_line[3]))
+  cla.append(words_line[7])
+  ########################### -modified
   while True:
     line=f.readline()
     if not line: break
@@ -41,24 +56,25 @@ def Read_rttm(rttmFile):
     time.append(float(words_line[3]))
     cla.append(words_line[7])
     endTime=float(words_line[3])+float(words_line[4])
+  time.append(endTime)
   f.close()
   # close rttm file
   return time, cla
 
 def Search_class(inputTime,time,cla):
   # search the input time to match the class
-  cnt=0;
+  cnt=0;  outClass='out of range';
   for n in time:
     if inputTime < n:
-      outClass=cla[cnt-1];
-      break;
-    cnt=cnt+1;
+      outClass=cla[cnt-1]
+      break
+    cnt=cnt+1
 
   return outClass
 
 if __name__=="__main__":
   rttmFile=Check_file(rttmFile="mask_00001_700K.rttm")
   time,cla=Read_rttm(rttmFile)
-  outClass=Search_class(inputTime=252,time=time,cla=cla)
+  outClass=Search_class(inputTime=3460.771,time=time,cla=cla)
   print outClass
 
