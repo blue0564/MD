1f796a25cda1e2f9ea102a63a083833ab7f55d15
Byeong-Yong Jang <darkbulls44@gmail.com>
2018-05-11 11:04:49 +0900

    adv_confusmat

--- cnn/make_confusion_matrix.py
+++ cnn/make_confusion_matrix.py
@@ -10,10 +10,16 @@ def main():
     usage = "%prog [options] <pred-lab-file> <confusion-matrix-file>"
     parser = OptionParser(usage)
 
+    parser.add_option('--num-classes', dest='nclasses',
+                      help='Number of classes [default: %default]',
+                      default=5, type='int')
+
     (o, args) = parser.parse_args()
     (pred_lab_file, confmat_file) = args
 
-    confmat = np.zeros((5,5),dtype=int)
+    nclasses = o.nclasses
+
+    confmat = np.zeros((nclasses,nclasses),dtype=int)
 
     with open(pred_lab_file,'r') as f:
         pred_lab_list = f.readlines()
@@ -26,9 +32,9 @@ def main():
     with open(confmat_file,'w') as f:
         f.write("Total number : %d\n"%(len(pred_lab_list)))
         f.write("pred\\refs :\t0\t1\t2\t3\t4\n")
-        for i in range(5):
+        for i in range(nclasses):
             f.write("\t")
-            for j in range(5):
+            for j in range(nclasses):
                 f.write("\t%d"%(confmat[i,j]))
             f.write("\n")
 
